071744a0e9c5e9a70c61c09ba83616fad18a0210
ParsaHY <m.parsahy@gmail.com>
2024-02-26 14:30:20 +0330

    Add new aliases

--- add_farsi_aliases.sh
+++ add_farsi_aliases.sh
@@ -4,6 +4,22 @@
 
 # Path to the bashrc file
 bashrc_file="$HOME/.bashrc"
+alias گربه='cat'
+alias لمس='touch'
+alias پک='rm'
+alias پکز='rm -rf'
+alias تپ='cd'
+alias خانه='cd ~'
+alias بالا='cd ..'
+alias پایین='cd -'
+alias چاپ='echo'
+alias گیت='git'
+alias کلون='git clone'
+alias اضافه='git add'
+alias ثبت='git commit'
+alias فشار='git push'
+alias بکش='git pull'
+alias چک='git status'
 
 # Check if the aliases already exist in the bashrc file
 if grep -Fxq "# Custom Aliases" "$bashrc_file"; then
